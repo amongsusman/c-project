5d8c5c466dc489a27512582104674073c311ed49
amongsusman <149024306+amongsusman@users.noreply.github.com>
2024-09-10 09:00:12 -0400

    Add files via upload

---
+++ proj1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string word;
+    int num;
+    char letter;
+    float dec;
+    bool val;
+    num = 3; letter = 'a'; dec = 3.14159; val = true;
+    word = "man";
+    cout << word << " likes " << letter << " and " << dec << " and " << val << "\n";
+}
+
+
